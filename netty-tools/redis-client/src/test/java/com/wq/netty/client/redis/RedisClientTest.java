b0a7c2e34012061c97e483d6297462cf4f96e4c0
MichaelPaul0416 <18357903752m0@sina.cn>
2020-10-20 11:11:37 +0800

    redis其他数据类型的操作demo提交

--- netty-tools/redis-client/src/test/java/com/wq/netty/client/redis/RedisClientTest.java
+++ netty-tools/redis-client/src/test/java/com/wq/netty/client/redis/RedisClientTest.java
@@ -4,11 +4,27 @@ import org.junit.Test;
 
 public class RedisClientTest {
 
-//    RedisClient redisClient = new RedisClient("49.235.253.133",6379);
-    RedisClient redisClient = new RedisClient("localhost",6379);
+    //    RedisClient redisClient = new RedisClient("49.235.253.133",6379);
+    RedisClient redisClient = new RedisClient("127.0.0.1", 6379);
 
     @Test
-    public void display(){
-        redisClient.execute("get s3");
+    public void display() {
+        redisClient.execute("get iic-authuser:1002226562");
+    }
+
+    @Test
+    public void hget() {
+        redisClient.execute(" hget framework java");
+    }
+
+    @Test
+    public void lpush() {
+        redisClient.execute("del score");
+        redisClient.execute("lpush score java golang shell python");
+        long len = redisClient.execute("llen score");
+        System.out.println("llen score:"+len);
+        for (int i = 0; i < len; i++) {
+            System.out.println("lindex[" + i + "]:" + redisClient.execute("lindex score " + (i + 1)));
+        }
     }
 }
